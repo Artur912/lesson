f13a4f2409229fbadcc493c5527f37803d5ddd32
Artur <arturartur4210@mail.ru>
2024-01-12 18:55:35 +0500

    задание урока массива

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] arr = new int[5]; //Int - тип даных челых чисел. arr - имя массива. new int [5] память на 5 чисел
+
+arr[0] = 5;  //arr - массив [0] индекс элемента
+arr[1] = 6;
+arr[2] = 1;
+arr[3] = 5;
+arr[4] = 3;
+
+Console.Write(arr[0]);//Console.Write - для вывода на экран. arr - для облащения. [0] - для для указание индекса
+Console.Write(arr[1]);
+Console.Write(arr[2]);
+Console.Write(arr[3]);
+Console.Write(arr[4]);
+
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5, }; // int[] - масив. arr2 - имя. new int [5] память на 5 элементов. {1, 2, 3, 4, 5,} - числа мисива.
+
+
+// arr = [1,2,3,4,5,] - образец на Phayton
